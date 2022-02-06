154666c9076652f820892c75f08ab2dfe4a2bfb3
Bruno Tarditi <brunotarditi@hotmail.com>
2022-02-05 22:02:46 -0300

    Corrigiendo problema de petición

--- universityservice/src/main/java/com/example/universityservice/feignclients/StudentFeignClient.java
+++ universityservice/src/main/java/com/example/universityservice/feignclients/StudentFeignClient.java
@@ -2,12 +2,14 @@ package com.example.universityservice.feignclients;
 
 import com.example.universityservice.models.Student;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @FeignClient(name = "studentservice")
-@RequestMapping("/students")
 public interface StudentFeignClient {
     @PostMapping()
     Student save(@RequestBody Student student);
--- universityservice/src/main/java/com/example/universityservice/feignclients/TeacherFeignClient.java
+++ universityservice/src/main/java/com/example/universityservice/feignclients/TeacherFeignClient.java
@@ -2,12 +2,14 @@ package com.example.universityservice.feignclients;
 
 import com.example.universityservice.models.Teacher;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @FeignClient(name = "teacherservice")
-@RequestMapping("/teachers")
 public interface TeacherFeignClient {
     @PostMapping()
     Teacher save(@RequestBody Teacher teacher);
