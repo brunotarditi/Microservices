3395054e8e1894ca9f0ffc9bb1971b5ed1147f84
Bruno Tarditi <brunotarditi@hotmail.com>
2022-02-06 21:46:39 -0300

    Modify feign client

--- universityservice/src/main/java/com/example/universityservice/feignclients/StudentFeignClient.java
+++ universityservice/src/main/java/com/example/universityservice/feignclients/StudentFeignClient.java
@@ -7,11 +7,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @FeignClient(name = "studentservice")
-@RequestMapping("/students")
 public interface StudentFeignClient {
-    @PostMapping()
+    @PostMapping("/students")
     Student save(@RequestBody Student student);
 
-    @GetMapping("/byUniversity/{universityId}")
+    @GetMapping("/students/byUniversity/{universityId}")
     List<Student> getStudents(@PathVariable long universityId);
 }
