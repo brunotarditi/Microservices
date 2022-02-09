23f4278d617c6965b4b885157d239388e6bb6765
Bruno Tarditi <brunotarditi@hotmail.com>
2022-02-09 00:19:56 -0300

    Implements authentication service with JWToken

--- authservice/src/main/java/com/example/authservice/security/JwtProvider.java
+++ authservice/src/main/java/com/example/authservice/security/JwtProvider.java
@@ -52,9 +52,7 @@ public class JwtProvider {
         }catch (Exception e){
             return false;
         }
-        if (!isAdmin(token) && routeValidator.isAdminPath(dto))
-            return false;
-        return true;
+        return isAdmin(token) || !routeValidator.isAdminPath(dto);
     }
 
     public String getUserNameFromToken(String token){
